896f7b0333ad90c9b2da107e05e2b3ef00cc14e7
Conor McMahon <cMcMahon@courthouse-technologies.com>
2021-02-24 13:04:07 -0800

    Add home path and remove static chromedriver

--- webui/urls.py
+++ webui/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [    
     path('', views.query, name='dc-query')
     , path('query_submit', views.query_submit, name='dc-query_submit')
+    , path('home', views.home, name='dc-home')
 ]
