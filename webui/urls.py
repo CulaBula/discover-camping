2ead478f676d7c1d75f6df6cfc3562bde08a0c80
Conor McMahon <cMcMahon@courthouse-technologies.com>
2021-02-24 12:36:42 -0800

    home change

--- webui/urls.py
+++ webui/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views 
 
-urlpatterns = [
-    path('', views.home, name='dc-home')
-    , path('query', views.query, name='dc-query')
-    , path('query_submit', views.query_submit, name='dc-query_submit')
+urlpatterns = [    
+    path('query', views.query, name='dc-query')
+    , path('', views.query_submit, name='dc-query_submit')
 ]
