7d4a27c96bf5a5ffde7e742d80356aac406e3d7d
Conor McMahon <cMcMahon@courthouse-technologies.com>
2021-02-24 12:43:55 -0800

    home page fix

--- webui/urls.py
+++ webui/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [    
-    path('query', views.query, name='dc-query')
-    , path('', views.query_submit, name='dc-query_submit')
+    path('', views.query, name='dc-query')
+    , path('query_submit', views.query_submit, name='dc-query_submit')
 ]
